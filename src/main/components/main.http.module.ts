6759cba4f4320a28fc6f05a8410ac6998eed7649
orgal <or.gal@vee.com>
2023-12-14 23:37:13 +0200

    fix: removed gateway

--- src/main/components/main.http.module.ts
+++ src/main/components/main.http.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from '@appstack-io/health';
 import { IdentityServicesModule } from '@appstack-io/identity';
-import { GatewayModule } from '@appstack-io/gateway';
 
 @Module({
-  imports: [IdentityServicesModule, HealthModule, GatewayModule],
+  imports: [IdentityServicesModule, HealthModule],
 })
 export class MainHttpModule {}
